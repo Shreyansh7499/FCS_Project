b8e7c36fde9fe02d8868e61a40d1fdf8c5c09268
Shreyansh Nagpal <30625967+Shreyansh7499@users.noreply.github.com>
2019-10-30 22:54:07 +0530

    updated settings

--- FCS_Project/settings.py
+++ FCS_Project/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'djangosecure.middleware.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django_otp.middleware.OTPMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +59,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_SSL_REDIRECT =True
+SECURE_HSTS_SECONDS = 15
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_FRAME_DENY = True
+SECURE_CONTENT_TYPE_NOSNIFF =True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE =True
+SESSION_COOKIE_HTTPONLY = True
 ROOT_URLCONF = 'FCS_Project.urls'
 
 TEMPLATES = [
