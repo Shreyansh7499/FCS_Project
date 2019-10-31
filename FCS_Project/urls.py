c5f4045ba076619cca7342bec98b1905ebb20fa8
Shreyansh Nagpal <30625967+Shreyansh7499@users.noreply.github.com>
2019-10-31 21:39:35 +0530

    Update urls.py

--- FCS_Project/urls.py
+++ FCS_Project/urls.py
@@ -84,8 +84,8 @@ urlpatterns = [
 
     path('password_reset/',auth_view.PasswordResetView.as_view(template_name='Users/password_reset.html'),name='password_reset'),
     path('password_reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='Users/password_reset_done.html'),name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
-    path('password_reset_complete/',auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='Users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password_reset_complete/',auth_view.PasswordResetCompleteView.as_view(template_name='Users/password_reset_complete.html'),name='password_reset_complete'),
 
 
     path('friends/',user_view.friend_page,name="friend_page"),
